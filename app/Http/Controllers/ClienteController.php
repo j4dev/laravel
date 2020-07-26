f5e42554e18bf5835e2cc46a9035fdbf250f1afb
j4dev <jesusguanga98@outlook.com>
2020-07-25 22:10:46 -0500

    Retiro de comentarios.

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -7,69 +7,32 @@ use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
     $clientes = cliente::all();
       return $clientes;  //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         $clientes = cliente::created($request->all());//
         return $clientes;
     }
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\cliente  $cliente
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(cliente $cliente)
     {
         return $cliente;//
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\cliente  $cliente
-     * @return \Illuminate\Http\Response
-     */
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\cliente  $cliente
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, cliente $cliente)
     {
         $cliente = cliente::updated($request->all());
         return $cliente;//
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\cliente  $cliente
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(cliente $cliente)
     {
         //
