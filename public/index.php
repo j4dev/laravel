493a49b8529d889a41343b9f18edce2d958cf41d
j4dev <jesusguanga98@outlook.com>
2020-07-25 21:34:27 -0500

    Inicial

--- public/index.php
+++ public/index.php
@@ -7,11 +7,6 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-
 define('LARAVEL_START', microtime(true));
 
 /*
